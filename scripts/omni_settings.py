24cb066db3afe2b548d534f95d69eca88977cfd9
yellowsoar <yellowsoar@gmail.com>
2021-10-31 23:00:16 +0800

    refactor:env_var generator

--- scripts/omni_settings.py
+++ scripts/omni_settings.py
@@ -2,17 +2,21 @@
 
 import os
 
-
-def update_dict(dictinary, key, value):
-    dictinary[key] = os.getenv(key, value)
-
-
-env_var = {}
-for _ in [
+env_var_default = [
     ["sfl_dir_csv", "csv"],
     ["sfl_dir_csv_icons", "csv_icons"],
     ["sfl_dir_icons", "icons"],
     ["sfl_dir_tmp", "tmp"],
     ["sfl_file_csv", "list.csv"],
-]:
+]
+
+
+def update_dict(dictionary, key, value):
+    dictionary[key] = os.getenv(key, value)
+
+
+
+env_var = {}
+
+for _ in env_var_default:
     update_dict(env_var, _[0], _[1])
